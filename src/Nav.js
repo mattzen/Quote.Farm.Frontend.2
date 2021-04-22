98cf16676d1acd34bb176e8610e7319680b976bd
Mateusz Zmijewski <mattzy90@hotmail.com>
2021-04-21 23:21:50 -0400

    nav link added

--- src/Nav.js
+++ src/Nav.js
@@ -2,26 +2,38 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Author from "./Author.js";
 import Authors from "./Authors.js";
+import Authors2 from "./Authors2.js";
 
 export default function Nav() {
   return (
     <Router>
       <div>
       <nav>
-      <Link to="/Authors">All Authors</Link>
+        <div>
+      <Link to="/Authors">Authors</Link>
+      </div>
+      <div>
+      <Link to="/Authors2">Authors</Link>
+      </div>
           {/* <Link to="/Authors/Adolf-Hitler">Adolf Hitler</Link>
           <Link to="/Authors/Albert-Einstein">Albert Einstein</Link> */}
         </nav>
         {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
+   
+       
+       <div>
         <Switch>
           <Route path="/Authors/:author" component = {Author}>
           </Route>
-          <Route path="/Authors/" component = {Authors}>
-          </Route>
           <Route path="/search/:searchPhrase" component = {Author}>
           </Route>
+          <Route path="/Authors/" component = {Authors}>
+          </Route>
+          <Route path="/Authors2/" component = {Authors2}>
+          </Route>
         </Switch>
+        </div>
       </div>
     </Router>
   );
