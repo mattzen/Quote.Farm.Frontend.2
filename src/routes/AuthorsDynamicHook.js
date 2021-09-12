162cef3fd923c2700351c2f9f6c8e2bff8f4bd0e
Mateusz Zmijewski <mattzy90@hotmail.com>
2021-09-12 12:36:53 -0400

    some refactoring

---
+++ src/routes/AuthorsDynamicHook.js
@@ -0,0 +1,106 @@
+import React from "react";
+import loading from "./loading.gif";
+import "./AuthorsDynamic.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+class AuthorsDynamic extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [[]],
+      alphabet: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ],
+      showLoader: true,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    //e.preventDefault();
+  }
+
+  componentDidMount() {
+    this.GetAuthors(this.handleClick);
+  }
+
+  GetAuthors = (handleEvent) => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    fetch(
+      "https://99tpj0um53.execute-api.us-east-2.amazonaws.com/dev/QuoteFarmApi-Test/Authors",
+      //"http://localhost:53886/authors/",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ authors: data }))
+      .then((_) => this.setState({ showLoader: false }));
+  };
+
+  getLoader = () => {
+    if (this.state.showLoader) {
+      return (
+        <div id="loading-div">
+          <img src={loading} alt="loading..." />
+        </div>
+      );
+    }
+  };
+
+  
+  render() {
+    return (
+      <div id="authors-table" class>
+        <div id="authors-table-header">All Authors</div>
+
+        <div id="flex-container" class="flex-container">
+          {this.getLoader()}
+          {this.state.authors.map(function (arg) {
+            return arg.map(function (author) {
+              let url = "/Authors/" + author.split(" ").join("-");
+
+              return (
+                <div class = "link-div">
+                  <Link to={url}>{author}</Link>
+                </div>
+              );
+
+            });
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AuthorsDynamic;
