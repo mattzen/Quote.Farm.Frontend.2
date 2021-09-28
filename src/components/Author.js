6b5aeffbd6ba1e91813bade83a0db5117d17054c
Mateusz Zmijewski <mattzy90@hotmail.com>
2021-09-28 12:34:48 -0400

    wiki link added to authors

--- src/components/Author.js
+++ src/components/Author.js
@@ -10,6 +10,7 @@ class Author extends React.Component {
       quotes: [],
       showLoader: true,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
   getLoader = () => {
     if (this.state.showLoader) {
@@ -50,10 +51,17 @@ class Author extends React.Component {
       .then((data) => this.setState({ quotes: data , showLoader : false}));
   };
 
+  handleClick(click){
+    window.open(click.target.id, '_blank');
+    window.focus();
+  }
+
+
   render() {
+    let wikiLink = "https://en.wikipedia.org/wiki/" + this.state.author.toString().split('-').join('_');
     return (
       <div id="authorPage">
-        <div id="author">{this.state.author.toString().split('-').join(' ')}</div>
+        <div id="author">{this.state.author.toString().split('-').join(' ')}<div ><a onClick={this.handleClick} id={wikiLink}>wiki</a></div></div>
         <div id="quotes">
           {this.getLoader()}
           {this.state.quotes.map(function (element, index) {
